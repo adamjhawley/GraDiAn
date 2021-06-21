75888f5a14d7dd65aa6728fb9ccd36b2a1eab1e7
adamjhawley <adamhawley99@gmail.com>
2021-06-21 10:35:58 +0300

    Added package level files

---
+++ gradian/__init__.py
@@ -0,0 +1,1 @@
+from sent_tree import SentTree
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+setup(name='Grammatical Distribution Analyser',
+      version='0.0.0.1',
+      description='Python Distribution Utilities',
+      author='Adam Hawley',
+      author_email='ajh651@york.ac.uk',
+    packages=setuptools.find_packages(),
+     )

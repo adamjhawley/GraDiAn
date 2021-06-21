b05334bd0e429e1918f4898187f3c5ff3cf79c10
adamjhawley <adamhawley99@gmail.com>
2021-06-21 12:25:02 +0300

    Refactored + linted count_trees

--- gradian/count_trees.py
+++ gradian/count_trees.py
@@ -1,23 +1,15 @@
 import argparse
-from typing import List
-import spacy
 import json
 import os
+from typing import List
+
+import spacy
 from pandas import read_csv
 from pandas.core.frame import DataFrame
-
-from sent_tree import SentTree
 from spacytextblob.spacytextblob import SpacyTextBlob
 from tqdm import tqdm
 
-
-nlp = spacy.load("en_core_web_trf",
-                exclude=['ner', 'attribute_ruler', 'lemmatizer',
-                         'token2vec'])  # Globally instantiated 
-
-use_spacy_text_blob = False
-if use_spacy_text_blob:
-    nlp.add_pipe('spacytextblob')
+from sent_tree import SentTree
 
 
 def get_tree_count(df: DataFrame, attribute: str):
@@ -43,6 +35,7 @@ def get_tree_count(df: DataFrame, attribute: str):
                                reverse=True))
     return sorted_trees
 
+
 def get_tree_count_arr(texts: List, attribute: str):
     trees = dict()
 
@@ -66,6 +59,7 @@ def get_tree_count_arr(texts: List, attribute: str):
                                reverse=True))
     return sorted_trees
  
+
 def get_tree_count_single(text: str, attribute: str):
     trees = dict()
 
@@ -85,6 +79,7 @@ def get_tree_count_single(text: str, attribute: str):
             trees[tree]['texts'] = [sent.text]
     return trees
 
+
 def main(args):
     testset_path = args.data_sets[0]
     trainset_path = args.data_sets[1]
@@ -93,20 +88,33 @@ def main(args):
     attribute = args.attribute
 
     test_trees = get_tree_count(df_test, attribute)
-    with open(os.path.join(args.output_path, f'test_count_{attribute}.json'), 'w') as f:
-        json.dump(test_trees, f, indent=2)
+    if args.output_path is not None:
+        with open(os.path.join(args.output_path, f'test_count_{attribute}.json'), 'w') as f:
+            json.dump(test_trees, f, indent=2)
 
     train_trees = get_tree_count(df_train, attribute)
-    with open(os.path.join(args.output_path, f'train_count_{attribute}.json'), 'w') as f:
-        json.dump(train_trees, f, indent=2)
+    if args.output_path is not None:
+        with open(os.path.join(args.output_path, f'train_count_{attribute}.json'), 'w') as f:
+            json.dump(train_trees, f, indent=2)
+
+    return
+
 
 if __name__ == "__main__":
+    nlp = spacy.load("en_core_web_trf",
+                     exclude=['ner', 'attribute_ruler', 'lemmatizer',
+                              'token2vec'])  # Globally instantiated 
+
     p = argparse.ArgumentParser()
     p.add_argument('-d', '--data_sets', type=str, nargs=2,
                    default=['datasets/SST-2/test.tsv',
                             'datasets/SST-2/train.tsv'])
-    p.add_argument('-o', '--output_path', type=str, default='generated')
+    p.add_argument('-o', '--output_path', type=str, default=None)
     p.add_argument('-a', '--attribute', type=str, default='pos')
+    p.add_argument('-t', '--text-blob', type=bool, default=False)
     args = p.parse_args()
+
+    if args.text_blob:
+        nlp.add_pipe('spacytextblob')
+
     main(args)
-    
